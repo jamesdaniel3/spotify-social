f2247258b58c5179baf12eea9807a389b9fba2b4
mlaflam <mlaflam@users.noreply.github.com>
2024-05-28 13:07:12 -0400

    Basic Search Bar

--- frontend/spotify-project/src/routes/DiscoverPage.jsx
+++ frontend/spotify-project/src/routes/DiscoverPage.jsx
@@ -1,9 +1,45 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import '../styles/discover.css';
+
+import searchIcon from '../icons/search-icon.png';
+
 const DiscoverPage = () => {
-    return(
-        <>
-            <p>DiscoverPage</p>
-        </>
-    )
+    
+  const [searchTerm, setSearchTerm] = useState("");
+  return(
+    <>
+      <div className='discover-page'>
+        <div className='header'>
+          <div className='left-section'>
+            <button>...</button>
+            <div className='discover-title'>discover</div>
+          </div>
+
+          <div className='center-section'>
+            <div className='search-container'>
+              <img src={searchIcon} className='search-icon'/>
+            <input
+              maxLength="100"
+              placeholder='Search users'
+              className='search-box'
+              type="text"
+              value={searchTerm} 
+              onChange={(e) => {
+                setSearchTerm(e.target.value); }}
+              />
+            </div>
+            
+
+          </div>
+
+            <div>
+              
+            </div>
+          
+        </div>
+        
+      </div>
+    </>
+  )
 }
 export default DiscoverPage;
