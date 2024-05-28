3a2574385d4c22b8b45e72a6f8f145fa51d181ac
mlaflam <mlaflam@users.noreply.github.com>
2024-05-28 14:35:59 -0400

    Styling search bar

--- frontend/spotify-project/src/routes/DiscoverPage.jsx
+++ frontend/spotify-project/src/routes/DiscoverPage.jsx
@@ -1,31 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/discover.css';
 
+import searchIconFocus from '../icons/search-icon-focus.png';
 import searchIcon from '../icons/search-icon.png';
 
 const DiscoverPage = () => {
-    
   const [searchTerm, setSearchTerm] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
   return(
     <>
       <div className='discover-page'>
         <div className='header'>
           <div className='left-section'>
-            <button>...</button>
             <div className='discover-title'>discover</div>
           </div>
 
           <div className='center-section'>
             <div className='search-container'>
-              <img src={searchIcon} className='search-icon'/>
-            <input
-              maxLength="100"
-              placeholder='Search users'
-              className='search-box'
-              type="text"
-              value={searchTerm} 
-              onChange={(e) => {
-                setSearchTerm(e.target.value); }}
+              <img src={isFocused ? searchIconFocus : searchIcon} className='search-icon' />
+              <input
+                maxLength="100"
+                placeholder='Search users'
+                className='search-box'
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
               />
             </div>
             
