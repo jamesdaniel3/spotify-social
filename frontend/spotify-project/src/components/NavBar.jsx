ae932d4f3200b53cec2474841425118e75d9900b
Stella Tsogtjargal <stella_tsogtjargal@brown.edu>
2024-05-28 10:50:26 -0500

    added navbar

--- frontend/spotify-project/src/components/NavBar.jsx
+++ frontend/spotify-project/src/components/NavBar.jsx
@@ -1,9 +1,39 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
+import '../styles/navbar.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
 const NavBar = () => {
-    return(
-        <>
-            <p>NavBar</p>
-        </>
-    )
+  return (
+    <>
+        <Navbar expand={false}>
+          <Container fluid>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${false}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton />
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link href="/">discover</Nav.Link>
+                    <Nav.Link href="/profile">profile</Nav.Link>
+                    <Nav.Link href="/messages">messages</Nav.Link>
+                    <Nav.Link href="/forums">forums</Nav.Link>
+                    <Nav.Link href="/liked-songs">liked songs</Nav.Link>
+                    <Nav.Link href="/top-artists">top artists</Nav.Link>
+                    <Nav.Link href="/top-songs">top songs</Nav.Link>
+                    <Nav.Link href="/forums">forums</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+    </>
+  );
 }
+
 export default NavBar;
