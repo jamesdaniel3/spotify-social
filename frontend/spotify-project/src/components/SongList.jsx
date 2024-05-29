449413ad35ff09ca0a367597b4fa74fab706addf
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-05-29 19:41:21 -0400

    removed unused import and added functionality to take user to the album of a song if they click on the album photo

--- frontend/spotify-project/src/components/SongList.jsx
+++ frontend/spotify-project/src/components/SongList.jsx
@@ -3,27 +3,21 @@ import Table from 'react-bootstrap/Table';
 import '../styles/song-list.css'
 
 const SongList = ({data}) => {
+    const handleImageClick = (url) => {
+        window.open(url, '_blank');
+    };
+
   return (
     <div className='tableDiv'>
       <Table borderless className='songs bigger-screens' variant='dark'>
-      {/*
-      I commented out table header:
-      <thead>
-        <tr className='head'>
-          <th>#</th>
-          <th>Title</th>
-          <th>Album</th>
-          <th>Artist</th>
-        </tr>
-      </thead>
-      */}
         <tbody>
           {data.map((song, idx)=>{
             return(
               <tr>
                   <td className='rounded-left'>{idx+1}</td>
                   <td>
-                    <img src = {song.album.images[0].url}/>
+                    <img src = {song.album.images[0].url}
+                         onClick={() => handleImageClick(song.album.external_urls.spotify)}/>
                   </td>
                   <td>{song.name}</td>
                   <td>{song.album.name}</td>
@@ -48,7 +42,9 @@ const SongList = ({data}) => {
               <tr>
                   <td className='rounded-left'>{idx}</td>
                   <td>
-                    <img src = {song.album.images[0].url}/>
+                    <img src = {song.album.images[0].url}
+                         onClick={() => handleImageClick(song.album.external_urls.spotify)}
+                    />
                   </td>
                   <td>{song.name}</td>
                   <td className='rounded-right'>
--- frontend/spotify-project/src/routes/TopArtists.jsx
+++ frontend/spotify-project/src/routes/TopArtists.jsx
@@ -2,15 +2,9 @@ import React from 'react';
 import TabbingArtists from '../components/TabbingArtists';
 import '../styles/topArtists.css'; 
 import Header from '../components/Header';
-import Tabbing from "../components/Tabbing.jsx";
 
 
 const TopArtists = ({ topArtistsShort, topArtistsMedium, topArtistsLong }) => {
-  console.log(topArtistsShort);
-  console.log(topArtistsMedium);
-  console.log(topArtistsLong);
-
-
   return (
       <>
         <Header title={"top artists"}/>
