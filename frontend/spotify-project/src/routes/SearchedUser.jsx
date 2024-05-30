9a1fb9be5d961f950864d158f680926a5a53d81a
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-05-30 18:17:33 -0400

    got header onto page

--- frontend/spotify-project/src/routes/SearchedUser.jsx
+++ frontend/spotify-project/src/routes/SearchedUser.jsx
@@ -1,12 +1,56 @@
 import {useParams} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import Header from "../components/Header.jsx";
+
+import ArtistList from "../components/ArtistList.jsx";
+import SongList from "../components/SongList.jsx";
+import SettingsModal from "../components/SettingsModal.jsx";
 
 export default function SearchedUser() {
     const {id} = useParams();
-    console.log(id)
+    const [firebaseInfo, setFirebaseInfo] = useState({});
 
+    const fetchUserData = async () => {
+
+        try {
+            const result = await axios.get(`http://localhost:8888/user/${id}`);
+            setFirebaseInfo(result.data);
+        } catch (error) {
+            console.error("Error checking for user:", error);
+        }
+    };
+
+    useEffect(() => {
+        if (id) {
+            fetchUserData();
+        }
+    }, [id]);
+
+    console.log(firebaseInfo)
+
+    // put in an on click to navigate to spotify profile, requires adding field to cloudbase
     return(
         <>
-            <p>This is a user page</p>
+            <>
+                <Header title={firebaseInfo.display_name + "'s profile"} />
+                <div className="profile-container">
+                    <div className={"profile-header"}>
+                        <img
+                            src={firebaseInfo.profilePicture || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" }
+                            alt={"profile picture"}
+                            className="profile-pic"
+                        />
+                    </div>
+                    <div className={"profile-info"}>
+                        <h2 className={"username"} style={{ color: "white" }}>
+                            {firebaseInfo.display_name}
+                        </h2>
+                        <p style={{ color: "white" }}>{firebaseInfo.followers} followers</p>
+                    </div>
+
+                </div>
+            </>
         </>
     )
 
