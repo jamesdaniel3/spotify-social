4fca64f2f63e20cff094f26143fbf73bf1a6b491
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2024-05-31 00:20:12 -0400

    made it so that you can't message yourself if you search your own profile

--- frontend/spotify-project/src/routes/SearchedUser.jsx
+++ frontend/spotify-project/src/routes/SearchedUser.jsx
@@ -33,7 +33,6 @@ export default function SearchedUser({ profileInfo }) {
 
     const handleSendMessage = async () => {
         try {
-            console.log(id)
             await axios.post('http://localhost:8888/api/sendMessage', {
                 current_user_id: current_user_id,
                 recipient_id: id,
@@ -56,7 +55,8 @@ export default function SearchedUser({ profileInfo }) {
                         className="profile-pic"
                         style={{ marginRight: '20px' }}
                     />
-                    {firebaseInfo.open_for_messages && (
+                    {firebaseInfo.open_for_messages &&
+                        id !== profileInfo.id &&(
                         <div className="message-input-container" style={{ display: 'flex', alignItems: 'center' }}>
                             <input
                                 type="text"
